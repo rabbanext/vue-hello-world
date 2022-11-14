c801c8bc75981437fe0975ff52c709560260f34b
rabbanos <59020772+rabbanos@users.noreply.github.com>
2022-11-15 06:11:00 +0700

    commit
    axios
    get pokeapi

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <div v-for="item, index in info" :key="index">
+    {{ item }}
+    <a :href="item.url">{{ item.name }}</a>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      info: null
+    }
+  },
   components: {
     HelloWorld
+  },
+  mounted() {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/')
+      .then(response => {
+        console.log(response)
+        this.info=response.data.results
+      })
   }
 }
 </script>
